503eaf61d01f38f6e2ed1a7f639d967eaffe4639
ignakio <nacho.cladera@hotmail.com>
2023-05-17 15:07:07 +0000

    Added new state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreeting from "./components/StatefulGreeting";
+import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting  greeting="I`m a stateful class component" />
+      <StatefulGreetingWithCallback  greeting="I`m a stateful class component" />
     </div>
   );
 }
